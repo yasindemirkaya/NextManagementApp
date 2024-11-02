489f612eb2e99cd81f6c690cf4b2c970d82a1eb1
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-11-02 19:15:41 +0300

    _app.js
    app.js updated

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/global.scss';
 
 import { Provider } from "react-redux";
-import store from "@/redux/store";
+import { persistor } from "@/redux/config"
+import { PersistGate } from 'redux-persist/integration/react'
+import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
+import store from "@/redux/store";
 import React from 'react';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </SessionProvider>
   );
 }
