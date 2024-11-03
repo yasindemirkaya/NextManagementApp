87b97c32fa8f4dc5c388b9dd04434160f6f82cb6
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-11-03 14:37:52 +0300

    header
    header component added, sign out added

---
+++ components/Common/Header/index.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useSession, signOut } from 'next-auth/react';
+import styles from './index.module.scss';
+
+const Header = () => {
+    const { data: session } = useSession();
+
+    // Session yoksa Headerı gösterme. Sadece login kullanıcılar için Header gösterilmeli.
+    if (!session) return null;
+
+    return (
+        <Navbar bg="light" expand="lg" className={styles.header}>
+            <Container>
+                <Navbar.Brand as={Link} href="/">
+                    Your Company
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} href="/">Home</Nav.Link>
+                        <Nav.Link as={Link} href="/about">About Us</Nav.Link>
+                        <Nav.Link as={Link} href="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} href="/privacy">Privacy Policy</Nav.Link>
+                    </Nav>
+                    <Nav className="ms-auto">
+                        {session ? (
+                            <Nav.Link onClick={() => signOut({ callbackUrl: '/' })}>
+                                Logout
+                            </Nav.Link>
+                        ) : (
+                            <Nav.Link as={Link} href="/login">Login</Nav.Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
--- components/Layouts/Default/index.js
+++ components/Layouts/Default/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from '@/components/Common/Footer';
+import Header from '@/components/Common/Header';
+
 import styles from './index.module.scss';
 
 const DefaultLayout = ({ children }) => {
     return (
         <div className={styles.layout}>
+            <Header />
             <main className={styles.mainContent}>{children}</main>
             <Footer />
         </div>
