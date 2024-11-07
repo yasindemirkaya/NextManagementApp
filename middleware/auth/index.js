26bb1bf780d86bf5bbd0629530f04b704af4f118
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-11-08 01:17:11 +0300

    middleware
    middleware update

--- middleware/auth/index.js
+++ middleware/auth/index.js
@@ -1,7 +1,7 @@
 import { verify } from 'jsonwebtoken';
 
 export const authenticateJWT = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1]; // 'Bearer token' formatında olmalı
+    const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
         return res.status(403).json({
             message: 'Access denied. No token provided.'
@@ -10,7 +10,7 @@ export const authenticateJWT = (req, res, next) => {
 
     verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(403).json({
+            return res.status(401).json({
                 message: 'Invalid or expired token.'
             });
         }
