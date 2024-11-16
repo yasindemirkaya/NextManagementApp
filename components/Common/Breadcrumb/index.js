e217cabe25f5163834d16d27610c1d2e82b7caea
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-11-16 15:29:12 +0300

    footer
    footer fixes

---
+++ components/Common/Breadcrumb/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Breadcrumb } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import styles from './index.module.scss';
+
+const BreadcrumbComponent = () => {
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+
+    if (!token) {
+        return null;
+    }
+
+    const router = useRouter();
+    const pathSegments = router.pathname.split('/').filter(Boolean);
+
+    // Breadcrumb linklerini formatlamak için
+    const segmentFormatter = (segment) => {
+        return segment.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    }
+
+    return (
+        <Breadcrumb className={styles.breadcrumb}>
+            <Breadcrumb.Item className={styles.breadcrumbItem} href="/">Home</Breadcrumb.Item>
+            {pathSegments.map((segment, index) => {
+                const segmentPath = `/${pathSegments.slice(0, index + 1).join('/')}`;
+
+                return (
+                    <Breadcrumb.Item
+                        key={index}
+                        active={index === pathSegments.length - 1}
+                        href={segmentPath}
+                        className={styles.breadcrumbItem}
+                    >
+                        {segmentFormatter(segment)}
+                    </Breadcrumb.Item>
+                );
+            })}
+        </Breadcrumb>
+    );
+};
+
+export default BreadcrumbComponent;
--- components/Common/Footer/index.js
+++ components/Common/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Link from 'next/link';
 import styles from './index.module.scss';
+import footerMenu from '@/static/components/footer';
 
 const Footer = () => {
     return (
@@ -13,12 +14,13 @@ const Footer = () => {
                             &copy; {new Date().getFullYear()} Your Company. All rights reserved.
                         </p>
                     </Col>
+
                     <Col md={6} className="d-flex justify-content-center justify-content-md-end h-100">
-                        <div className={styles.links}>
-                            <Link href="/about" className="me-3">About Us</Link>
-                            <Link href="/contact" className="me-3">Contact</Link>
-                            <Link href="/privacy">Privacy Policy</Link>
-                        </div>
+                        {footerMenu.map((menu) => (
+                            <div className={styles.links}>
+                                <Link href={menu.link} className="me-3">{menu.name}</Link>
+                            </div>
+                        ))}
                     </Col>
                 </Row>
             </Container>
