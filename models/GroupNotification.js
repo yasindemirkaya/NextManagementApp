8e42118d2326d9c8b3deca50f90a6294e2104119
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-12-23 16:46:18 +0300

    notifications-1
    Notification models are created.

---
+++ models/GroupNotification.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const groupNotificationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    group: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserGroup',
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: false,
+    },
+    is_seen: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+// 'GroupNotification' modelini oluşturur ve 'groupNotifications' koleksiyonuna bağlar
+const GroupNotification = mongoose.models.GroupNotification || mongoose.model('GroupNotification', groupNotificationSchema, 'groupNotifications');
+
+export default GroupNotification;
---
+++ models/PersonalNotification.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const personalNotificationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: false,
+    },
+    is_seen: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+// 'PersonalNotification' modelini oluşturur ve 'personalNotifications' koleksiyonuna bağlar
+const PersonalNotification = mongoose.models.PersonalNotification || mongoose.model('PersonalNotification', personalNotificationSchema, 'personalNotifications');
+
+export default PersonalNotification;
