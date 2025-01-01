ec20013aab9b1cabd0a4f5410bd017dd7a1ff178
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-01-01 21:06:45 +0300

    guid-integration
    every service response has guid now

--- middleware/private/index.js
+++ middleware/private/index.js
@@ -1,24 +1,33 @@
 import verifyToken from './verifyToken';
 import privateRateLimit from './rateLimit';
-import logger from './logger'
+import logger from './logger';
 import connectToDatabase from '@/config/db';
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function applyMiddlewares(handler) {
    return async (req, res) => {
-      const middlewareArray = [connectToDatabase, privateRateLimit, verifyToken, logger]
+      const middlewareArray = [connectToDatabase, privateRateLimit, verifyToken, logger];
 
       for (const middleware of middlewareArray) {
          await new Promise((resolve, reject) => {
             middleware(req, res, (err) => {
                if (err) {
-                  reject(err)
+                  reject(err);
                } else {
-                  resolve()
+                  resolve();
                }
             });
          });
       }
-      await handler(req, res)
-   }
+
+      const originalJson = res.json;
+      res.json = (body) => {
+         const createdAt = new Date().toISOString();
+         const guid = `${uuidv4()}-${createdAt}`;
+         body.guid = guid;
+         return originalJson.call(res, body);
+      };
+
+      await handler(req, res);
+   };
 }
