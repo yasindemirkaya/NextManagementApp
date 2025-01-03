bb0d74d26ffa1262908ffa6c374ef4c86f003311
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-01-04 01:18:31 +0300

    new-font
    New google font is added to the _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -13,23 +13,32 @@ import { useRouter } from 'next/router';
 import DefaultLayout from '@/components/Layouts/Default/index';
 import NotFoundLayout from '@/components/Layouts/404';
 
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const Layout = router.pathname === '/404' ? NotFoundLayout : DefaultLayout;
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Next Management App</title>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PersistGate>
-    </Provider>
+    <div className={montserrat.className}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Next Management App</title>
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
+      </Provider>
+    </div>
+
   );
 }
