20ceb71e5b269b8ea090b772c839761fef6e42d8
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-01-28 13:40:58 +0300

    404-error-multilang
    404 Page multilanguage added.

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,10 +1,12 @@
-import { Container, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import styles from './index.module.scss';
 import NotFoundLayout from '@/components/Layouts/404';
+import { useTranslations } from 'next-intl';
 
 const NotFoundPage = () => {
     const router = useRouter();
+    const t = useTranslations();
 
     const goHome = () => {
         router.push('/');
@@ -13,15 +15,27 @@ const NotFoundPage = () => {
     return (
         <NotFoundLayout>
             <h1 className={`text-primary ${styles.heading}`}>404</h1>
-            <h3 className={styles.subheading}>Page Not Found</h3>
+            <h3 className={styles.subheading}>{t("Page Not Found")}</h3>
             <p className={styles.description}>
-                Oops! The page you're looking for doesn't exist or has been moved.
+                {t("Oops! The page you're looking for doesn't exist or has been moved")}
             </p>
             <Button className={styles.buttonClass} onClick={goHome} variant="primary" size="lg">
-                Go to Home
+                {t("Go to Home")}
             </Button>
         </NotFoundLayout>
     );
 };
+
+export async function getStaticProps(context) {
+    const commonMessages = await import(`../../public/locales/common/${context.locale}.json`);
+
+    return {
+        props: {
+            messages: {
+                ...commonMessages.default
+            },
+        },
+    };
+}
 
 export default NotFoundPage;
