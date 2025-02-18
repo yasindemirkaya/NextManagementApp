a9c76ae57afd1f699530eda41307055e6df0563a
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-02-18 15:15:05 +0300

    capitalize-first-letter-fix
    capitalizeFirstLetter helper function updated.

--- helpers/capitalizeFirstLetter.js
+++ helpers/capitalizeFirstLetter.js
@@ -1,4 +1,4 @@
 export const capitalizeFirstLetter = (str) => {
     if (!str) return '';
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    return str.charAt(0).toUpperCase() + str.slice(1);
 };
