e6cbb622b54be43b39d0902ff59977055e2ee496
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-04-26 22:12:46 +0300

    env-files
    Local and Prod env files created. Gitignore and db configuration updated.

--- config/db.js
+++ config/db.js
@@ -6,11 +6,18 @@ const connectToDatabase = async (req, res, next) => {
     }
     try {
         const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ManagementApp';
+
         if (!dbUri) {
             throw new Error('MongoDB URI is not defined in environment variables');
         }
 
-        await mongoose.connect(dbUri);
+        // Veritabanı adı belirtmek iyi olabilir (Atlas'ta hangi db'yi kullanacağımızı belirleyebiliriz)
+        await mongoose.connect(dbUri, {
+            dbName: process.env.MONGODB_DB_NAME || 'NextManagementApp', // Veritabanı adı ekledik
+            useNewUrlParser: true, // Modern URL parser kullanmak
+            useUnifiedTopology: true, // Topology management
+        });
+
         console.log('MongoDB connection successful');
         next(); // Bağlantı başarılıysa bir sonraki middleware'a geç
     } catch (error) {
